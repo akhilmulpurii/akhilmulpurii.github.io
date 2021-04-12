30c02f462861374bed9d66e550b20f190accedcb
Akhil <akhil@f22labs.com>
2021-04-12 16:13:10 +0530

    remove dependency

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-import { Link, animateScroll as scroll } from "react-scroll";
 import { navigate } from "gatsby-link";
 
 // markup
