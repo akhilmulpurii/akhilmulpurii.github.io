12ee7aceeacc0ee31168503ecfebc5a9d350ce41
Akhil <akhil@f22labs.com>
2021-04-12 15:56:24 +0530

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="inner-footer">
+        <div className="footer-items">
+          <h1>Mulpuri Sai Akhil</h1>
+          <p className="designation">Frontend Developer.</p>
+        </div>
+
+        <div className="footer-items">
+          <h3>Quick Links</h3>
+          <div className="border1"></div>
+          <ul>
+            <a href="https://www.linkedin.com/in/akhil-mulpuri-94634b166/">
+              <li>LinkedIn</li>
+            </a>
+            <a href="https://www.facebook.com/SaiAkhilMulpz/">
+              <li>Facebook</li>
+            </a>
+            <a href="https://twitter.com/FartVader02">
+              <li>Twitter</li>
+            </a>
+            <a href="https://www.youtube.com/channel/UC5r8Fd1h17Eb-7Mong-ff4g">
+              <li>Youtube</li>
+            </a>
+          </ul>
+        </div>
+
+        <div className="footer-items">
+          <h3>More Links</h3>
+          <div className="border1"></div>
+          <ul>
+            <a href="https://github.com/akhilmulpurii">
+              <li>GitHub</li>
+            </a>
+            <a href="https://stackoverflow.com/users/10244850/akhil-mulpuri">
+              <li>Stack Overflow</li>
+            </a>
+            <a href="https://dev.to/akhilmulpurii">
+              <li>Dev.io</li>
+            </a>
+            <a href="https://medium.com/@akhilfilms02">
+              <li>Medium</li>
+            </a>
+          </ul>
+        </div>
+
+        <div className="footer-items">
+          <h3>Contact me</h3>
+          <div className="border1"></div>
+          <ul>
+            <li>
+              <i className="fa fa-map-marker" aria-hidden="true"></i>Hyderabad,
+              India - 500094
+            </li>
+            <li>
+              <i className="fa fa-phone" aria-hidden="true"></i>+91 8125125999
+            </li>
+            <li>
+              <i className="fa fa-envelope" aria-hidden="true"></i>
+              akhilfilms02@gmail.com
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        Akhil Mulpuri {new Date().getFullYear()}.
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import HeroSection from "../components/HeroSection";
 import "../main.css";
 import QuickViewSection from "../components/QuickViewSection";
 import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
 
 // markup
 const IndexPage = () => {
@@ -13,6 +14,7 @@ const IndexPage = () => {
       <HeroSection />
       <QuickViewSection />
       <ContactForm />
+      <Footer />
     </main>
   );
 };
