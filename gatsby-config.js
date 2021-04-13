d5dbeaf472b0a20365f8999044df662f524051ab
Akhil <akhil@f22labs.com>
2021-04-13 16:40:32 +0530

    added header for portfolio list

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -3,6 +3,7 @@ import "../../main.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { Helmet } from "react-helmet";
+import ProjectsListHero from "../../components/ProjectsListHero";
 
 // markup
 const IndexPage = () => {
@@ -13,7 +14,7 @@ const IndexPage = () => {
         <title>My Projects</title>
       </Helmet>
       <Header />
-
+      <ProjectsListHero />
       <Footer />
     </main>
   );
