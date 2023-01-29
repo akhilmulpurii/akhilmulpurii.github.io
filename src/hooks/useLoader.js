b4eb2e1ba07a10e4a67e209aa08b000142d846b5
Akhil Mulpuri <akhil@f22labs.com>
2023-01-29 16:00:05 -0600

    initialized nextjs

--- src/hooks/useLoader.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { LoaderContext } from "../providers/LoaderProvider";
-
-export default function useLoader() {
-  const { loader, setLoader } = useContext(LoaderContext);
-  return { loader, setLoader };
-}
