4205d84d14d2b96acca121c9378a2d3c866e55ee
Akhil Mulpuri <akhil@f22labs.com>
2023-01-29 20:29:14 -0600

    import issue fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import QuickViewSection from "../components/QuickViewSection";
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 import AboutMe from "../components/AboutMe";
-import data from "../../../helpers/projects.json";
+import data from "../helpers/projects.json";
 const projects = data?.projects || [];
 
 const IndexPage = ({ projects = [] }) => {
