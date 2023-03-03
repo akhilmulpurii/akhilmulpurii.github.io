548d2849de1a035a92db3ae6eedf61a4ee71e123
Akhil Mulpuri <akhil@f22labs.com>
2023-03-02 23:56:55 -0600

    details hero

--- src/components/ProjectDetailsHero/index.js
+++ src/components/ProjectDetailsHero/index.js
@@ -1,34 +1,228 @@
 import * as React from "react";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
 // markup
 const ProjectDetailsHero = ({ title, background, platform }) => {
   return (
-    <motion.section
-      style={{ background }}
-      className="project-details-hero-section"
+    <Section
       initial={{ opacity: 0, scale: 1.2 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1, delay: 0.5 }}
+      background={background}
     >
-      <div class="container">
-        <div class="box">
-          <div class="title">
-            <span class="block"></span>
-            <h1>
+      <Container>
+        <Box>
+          <Title>
+            <Block />
+            <Heading>
               {title}
-              <span></span>
-            </h1>
-          </div>
+              <span />
+            </Heading>
+          </Title>
 
-          <div class="role">
-            <div class="block"></div>
-            <p>{platform}</p>
-          </div>
-        </div>
-      </div>
-    </motion.section>
+          <Role>
+            <RoleBlock />
+            <Platform>{platform}</Platform>
+          </Role>
+        </Box>
+      </Container>
+    </Section>
   );
 };
 
 export default ProjectDetailsHero;
+
+const Section = styled(motion.section)`
+  overflow: hidden;
+  ${({ background }) =>
+    background &&
+    css`
+      background: ${background};
+    `}
+
+  @keyframes mainBlock {
+    0% {
+      width: 0%;
+      left: 0;
+    }
+    50% {
+      width: 100%;
+      left: 0;
+    }
+    100% {
+      width: 0;
+      left: 100%;
+    }
+  }
+
+  @keyframes secBlock {
+    0% {
+      width: 0%;
+      left: 0;
+    }
+    50% {
+      width: 100%;
+      left: 0;
+    }
+    100% {
+      width: 0;
+      left: 100%;
+    }
+  }
+
+  @keyframes mainFadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes popIn {
+    0% {
+      width: 0px;
+      height: 0px;
+      background: #e9d856;
+      border: 0px solid #ddd;
+      opacity: 0;
+    }
+    50% {
+      width: 10px;
+      height: 10px;
+      background: #e9d856;
+      opacity: 1;
+      bottom: 45px;
+    }
+    65% {
+      width: 7px;
+      height: 7px;
+      bottom: 0px;
+      width: 15px;
+    }
+    80% {
+      width: 10px;
+      height: 10px;
+      bottom: 20px;
+    }
+    100% {
+      width: 7px;
+      height: 7px;
+      background: #e9d856;
+      border: 0px solid #222;
+      bottom: 13px;
+    }
+  }
+
+  @keyframes secFadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const Block = styled.div`
+  width: 0%;
+  height: inherit;
+  background: #ffb510;
+  position: absolute;
+  animation: mainBlock 2s cubic-bezier(0.74, 0.06, 0.4, 0.92) forwards;
+  display: flex;
+`;
+
+const Box = styled.div`
+  width: 250px;
+  height: 250px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 40vh;
+  background: #232323;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 940px) {
+    height: 25vh;
+  }
+`;
+
+const Title = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 50px;
+`;
+
+const Heading = styled.div`
+  font-family: "Poppins";
+  color: #fff;
+  font-size: 32px;
+  -webkit-animation: mainFadeIn 2s forwards;
+  -o-animation: mainFadeIn 2s forwards;
+  animation: mainFadeIn 2s forwards;
+  animation-delay: 1.6s;
+  opacity: 0;
+  display: flex;
+  align-items: baseline;
+  position: relative;
+
+  & span {
+    width: 0px;
+    height: 0px;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+
+    background: #ffb510;
+    -webkit-animation: load 0.6s cubic-bezier(0.74, 0.06, 0.4, 0.92) forwards;
+    animation: popIn 0.8s cubic-bezier(0.74, 0.06, 0.4, 0.92) forwards;
+    animation-delay: 2s;
+    margin-left: 5px;
+    margin-top: -10px;
+    position: absolute;
+    bottom: 13px;
+    right: -12px;
+  }
+`;
+
+const Role = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 30px;
+  margin-top: -10px;
+`;
+
+const RoleBlock = styled.div`
+  width: 0%;
+  height: inherit;
+  background: #e91e63;
+  position: absolute;
+  animation: secBlock 2s cubic-bezier(0.74, 0.06, 0.4, 0.92) forwards;
+  animation-delay: 2s;
+  display: flex;
+`;
+
+const Platform = styled.p`
+  animation: secFadeIn 2s forwards;
+  animation-delay: 3.2s;
+  opacity: 0;
+  font-weight: 400;
+  font-family: "Lato";
+  color: #ffffff;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+`;
