32a446e06597375787215c1a44bafba1c2efb151
Akhil Mulpuri <akhil@f22labs.com>
2023-03-03 00:04:24 -0600

    tech stack

--- src/components/TechStack/index.js
+++ src/components/TechStack/index.js
@@ -1,38 +1,103 @@
 import { motion } from "framer-motion";
 import * as React from "react";
+import styled, { css } from "styled-components";
 
 const TechStack = ({ technologies = [], background }) => {
   return (
-    <motion.section
-      className="tech-stack-container"
+    <Container
       initial={{ opacity: 0, scale: 1.1 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 1, delay: 0.7 }}
-      style={{ background }}
+      background={background}
     >
-      <div className="tech-stack-wrapper">
-        <motion.span
-          className="tech-stack-title"
+      <Wrapper>
+        <Text
           initial={{ opacity: 0, scale: 1.1 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 0.8 }}
         >
           TECH STACK
-        </motion.span>
+        </Text>
         {technologies.map((tech, index) => (
-          <motion.div
-            className="tech-stack-chip-item"
+          <Chip
             initial={{ opacity: 0, scale: 1.1 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1, delay: 0.7 + index * 0.3 }}
             key={tech}
           >
             {tech}
-          </motion.div>
+          </Chip>
         ))}
-      </div>
-    </motion.section>
+      </Wrapper>
+    </Container>
   );
 };
 
 export default TechStack;
+
+const Container = styled(motion.section)`
+  display: flex;
+  width: 100%;
+  ${({ background }) =>
+    background &&
+    css`
+      background: ${background};
+    `}
+`;
+
+const Wrapper = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 20px;
+
+  @media only screen and (max-width: 940px) {
+    flex-direction: column;
+  }
+`;
+
+const Chip = styled(motion.div)`
+  background-color: #141414;
+  padding: 10px 20px;
+  margin: 0 10px;
+  border-radius: 5px;
+  display: flex;
+  @media only screen and (max-width: 940px) {
+    margin: 4px 0;
+  }
+`;
+
+const Text = styled(motion.span)`
+  color: #141414;
+  font-size: 24px;
+  font-weight: 300;
+  padding-right: 20px;
+  position: relative;
+  &:after {
+    content: "";
+    height: 100%;
+    width: 1px;
+    background-color: #141414;
+    display: flex;
+    position: absolute;
+    right: 6px;
+    top: 0;
+    bottom: 0;
+  }
+  @media only screen and (max-width: 940px) {
+    padding-right: 0;
+    padding-bottom: 20px;
+    &:after {
+      height: 1px;
+      width: 100%;
+      right: 0;
+      left: 0;
+      bottom: 12px;
+      top: auto;
+    }
+  }
+`;
