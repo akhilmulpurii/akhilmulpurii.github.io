17c200ed761ff04d2affe820b266ebc2269a2cb6
Akhil Mulpuri <akhil@f22labs.com>
2023-03-02 23:29:27 -0600

    fixed contact form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,6 +1,7 @@
 import { Form, Formik } from "formik";
 import { motion } from "framer-motion";
 import * as React from "react";
+import styled from "styled-components";
 import { ContactUsSchema } from "../../helpers/validationSchema";
 import Firebase from "../../service/firebase";
 import Snackbar from "node-snackbar";
@@ -34,10 +35,9 @@ const ContactForm = () => {
   }
 
   return (
-    <section className="quick-view-section">
+    <>
       <div id="contact" style={{ paddingBottom: 50 }}></div>
-      <motion.section
-        className="contact-form-section"
+      <Section
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -46,8 +46,7 @@ const ContactForm = () => {
           ease: "easeOut",
         }}
       >
-        <motion.h1
-          className="contact-form-title"
+        <Heading
           initial={{ opacity: 0, scale: 1.2, y: 20 }}
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{
@@ -57,7 +56,7 @@ const ContactForm = () => {
           }}
         >
           Get in touch
-        </motion.h1>
+        </Heading>
         <Formik
           initialValues={{ fullName: "", email: "", message: "" }}
           enableReinitialize
@@ -74,8 +73,8 @@ const ContactForm = () => {
             touched,
             values,
           }) => (
-            <Form className="contact-form-styled" onSubmit={handleSubmit}>
-              <div className="contact-form-left-section">
+            <StyledForm onSubmit={handleSubmit}>
+              <Left>
                 <motion.img
                   initial={{ opacity: 0, scale: 1.2, y: 20 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
@@ -86,10 +85,9 @@ const ContactForm = () => {
                   }}
                   src={"/images/envelope.png"}
                 />
-              </div>
-              <div className="contact-form-right-section">
-                <motion.div
-                  className="contact-form-input-container"
+              </Left>
+              <Right>
+                <InputContainer
                   initial={{ opacity: 0, scale: 1.2, y: 20 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
                   transition={{
@@ -108,13 +106,10 @@ const ContactForm = () => {
                     value={values.fullName}
                   />
                   {errors.fullName && touched.fullName && (
-                    <div className="contact-form-error-message">
-                      {errors.fullName}
-                    </div>
+                    <ErrorMessage>{errors.fullName}</ErrorMessage>
                   )}
-                </motion.div>
-                <motion.div
-                  className="contact-form-input-container"
+                </InputContainer>
+                <InputContainer
                   initial={{ opacity: 0, scale: 1.2, y: 20 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
                   transition={{
@@ -135,9 +130,8 @@ const ContactForm = () => {
                   {errors.email && touched.email && (
                     <ErrorMessage>{errors.email}</ErrorMessage>
                   )}
-                </motion.div>
-                <motion.div
-                  className="contact-form-input-container"
+                </InputContainer>
+                <InputContainer
                   initial={{ opacity: 0, scale: 1.2, y: 20 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
                   transition={{
@@ -158,20 +152,128 @@ const ContactForm = () => {
                   {errors.message && touched.message && (
                     <ErrorMessage>{errors.message}</ErrorMessage>
                   )}
-                </motion.div>
-                <motion.button
-                  className="contact-form-ripple-button"
-                  onClick={handleSubmit}
-                >
-                  Submit
-                </motion.button>
-              </div>
-            </Form>
+                </InputContainer>
+                <RippleButton onClick={handleSubmit}>Submit</RippleButton>
+              </Right>
+            </StyledForm>
           )}
         </Formik>
-      </motion.section>
-    </section>
+      </Section>
+    </>
   );
 };
 
 export default ContactForm;
+
+const Section = styled(motion.section)`
+  display: flex;
+  flex-direction: column;
+  max-width: 1360px;
+  margin: 0 auto;
+  background-color: #545454;
+  padding: 40px;
+  border-radius: 10px;
+
+  @media only screen and (max-width: 1440px) {
+    max-width: calc(100vw - 40px);
+    margin: 0 20px;
+    padding: 20px;
+  }
+  @media only screen and (max-width: 940px) {
+  }
+  margin-bottom: 60px;
+  margin-top: 70px;
+`;
+
+const Heading = styled(motion.h1)`
+  text-align: center;
+  margin: 0px;
+  font-size: 3vw;
+  margin-bottom: 30px;
+  @media only screen and (max-width: 640px) {
+    font-size: 5vw;
+  }
+`;
+
+const StyledForm = styled(Form)`
+  display: flex;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 640px) {
+    display: none;
+  }
+`;
+
+const Right = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const ErrorMessage = styled.div`
+  position: absolute;
+  bottom: 10px;
+  color: #ffc837;
+  font-size: 12px;
+  font-weight: bold;
+`;
+
+const InputContainer = styled(motion.div)`
+  position: relative;
+  padding-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  label {
+    font-size: 14px;
+    margin-bottom: 5px;
+    font-weight: 500;
+    span {
+      color: #fd4c59;
+      font-size: 18px;
+    }
+  }
+  input,
+  textarea {
+    background-color: #e6e6e6;
+    outline: none;
+    border: none;
+    box-shadow: none;
+    border-radius: 5px;
+    padding: 20px;
+    @media only screen and (max-width: 640px) {
+      padding: 14px;
+    }
+  }
+  textarea {
+    min-height: 120px;
+  }
+`;
+
+const RippleButton = styled(motion.button)`
+  background-position: center;
+  border: none;
+  border-radius: 2px;
+  padding: 12px 18px;
+  font-size: 16px;
+  text-transform: uppercase;
+  cursor: pointer;
+  color: white;
+  background-color: #846add;
+  outline: none;
+  transition: background 0.8s;
+  &:hover {
+    background: #947aed radial-gradient(circle, transparent 1%, #947aed 1%)
+      center/15000%;
+  }
+  &:active {
+    background-color: #745acd;
+    background-size: 100%;
+    transition: background 0s;
+  }
+`;
