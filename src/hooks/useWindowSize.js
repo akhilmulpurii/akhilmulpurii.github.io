5b6c494570df9739cf2d913b498917e44563b737
Akhil Mulpuri <akhil@f22labs.com>
2023-03-06 04:09:59 -0600

    trigger deploy

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -7,7 +7,6 @@ export default function useWindowSize() {
     width: undefined,
     height: undefined,
   });
-
   useEffect(() => {
     // only execute all the code below in client side
     // Handler to call on window resize
