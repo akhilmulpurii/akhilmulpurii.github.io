d34ed7d4803f24135268731ac6695a265e4ea0a9
Akhil Mulpuri <akhil@f22labs.com>
2023-03-06 02:11:55 -0600

    put cards on maintenance

---
+++ src/pages/blogs.js
@@ -0,0 +1,5 @@
+import MaintenanceCard from "@/components/MaintenanceCard/MaintenanceCard";
+
+export default function Blogs() {
+  return <MaintenanceCard />;
+}
---
+++ src/pages/contact.js
@@ -0,0 +1,5 @@
+import MaintenanceCard from "@/components/MaintenanceCard/MaintenanceCard";
+
+export default function Contact() {
+  return <MaintenanceCard />;
+}
