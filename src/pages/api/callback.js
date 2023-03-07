4ddfc24ea0290465d02f9b908e4b0432044ccaf9
Akhil Mulpuri <akhil@f22labs.com>
2023-03-06 18:10:31 -0600

    test

--- src/pages/api/callback.js
+++ src/pages/api/callback.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { AuthorizationCode } from "simple-oauth2";
 import { config } from "../../lib/config";
+import dedent from "dedent";
 
 /* eslint-disable require-jsdoc */
 export default async (req, res) => {
@@ -17,7 +18,7 @@ export default async (req, res) => {
     const accessToken = await client.getToken(tokenParams);
     const token = accessToken.token["access_token"];
 
-    const responseBody = renderBody("success", {
+    const responseBody = renderResponse("success", {
       token,
       provider,
     });
@@ -26,24 +27,46 @@ export default async (req, res) => {
     res.end(responseBody);
   } catch (e) {
     res.statusCode = 200;
-    res.end(renderBody("error", e));
+    res.end(renderResponse("error", e));
   }
 };
 
-function renderBody(status, content) {
-  return `
-    <script>
-      const receiveMessage = (message) => {
-        window.opener.postMessage(
-          'authorization:${content.provider}:${status}:${JSON.stringify(
-    content
-  )}',
-          message.origin
-        );
-        window.removeEventListener("message", receiveMessage, false);
-      }
-      window.addEventListener("message", receiveMessage, false);
-      window.opener.postMessage("authorizing:${content.provider}", "*");
-    </script>
+export function renderResponse(status, content) {
+  return dedent`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Authorizing ...</title>
+    </head>
+    <body>
+      <p id="message"></p>
+      <script>
+        console.log(${status}, ${content}
+        // Output a message to the user
+        function sendMessage(message) {
+          document.getElementById("message").innerText = message;
+          document.title = message
+        }
+
+        // Handle a window message by sending the auth to the "opener"
+        function receiveMessage(message) {
+          console.debug("receiveMessage", message);
+          window.opener.postMessage(
+            'authorization:github:${status}:${JSON.stringify(content)}',
+            message.origin
+          );
+          window.removeEventListener("message", receiveMessage, false);
+          sendMessage("Authorized, closing ...");
+        }
+
+        sendMessage("Authorizing ...");
+        window.addEventListener("message", receiveMessage, false);
+
+        console.debug("postMessage", "authorizing:github", "*")
+        window.opener.postMessage("authorizing:github", "*");
+      </script>
+    </body>
+  </html>
   `;
 }
