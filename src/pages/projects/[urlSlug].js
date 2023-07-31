a3e408ec88a1a6bb58b17feac397bab94b8912bf
akhilmulpurii <42495505+akhilmulpurii@users.noreply.github.com>
2023-07-30 19:50:30 -0500

    Update [urlSlug].js

--- src/pages/projects/[urlSlug].js
+++ src/pages/projects/[urlSlug].js
@@ -43,7 +43,7 @@ export const getStaticPaths = async () => {
   }
 }
 
-export async function getServerSideProps({ params }) {
+export function getStaticProps({ params }) {
 
   let project = projects.find(p => p.urlSlug === params.urlSlug);
     return {
