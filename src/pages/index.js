573f865384782ed210d8a9c1d9a2412651e75a82
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-02-08 16:30:25 -0600

    remove unwanted code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,9 @@
 import * as React from "react";
 import { attributes } from "../../content/home.md";
 import Sidebar from "@/widgets/Sidebar";
-import HeroSection from "@/components/HeroSection";
 
 const IndexPage = ({ staticContent = [] }) => {
-  return (
-    <Sidebar>
-      <HeroSection />
-      <HeroSection />
-      <HeroSection />
-      <HeroSection />
-    </Sidebar>
-  );
+  return <Sidebar></Sidebar>;
 };
 
 export async function getStaticProps() {
