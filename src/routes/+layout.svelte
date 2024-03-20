9f5023b49be82ee07be2a428ed6995fbe82a7dc1
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-03-20 18:45:03 -0500

    add cursor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
+<div
+	style="background-color: hsl(180,100%,50%);mix-blend-mode: difference;"
+	class="w-3 h-3 absolute rounded-full z-50 pointer-events-none select-none"
+	id="mouse-trail"
+/>
 <slot />
