7e6a7db5b06f29f4736e81b02771d2b5f1183551
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-03-20 18:58:29 -0500

    remove cursor trail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,4 @@
 	import '../app.css';
 </script>
 
-<div
-	style="background-color: hsl(180,100%,50%);mix-blend-mode: difference;"
-	class="w-3 h-3 absolute rounded-full z-50 pointer-events-none select-none"
-	id="mouse-trail"
-/>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
 	import HeroSection from '$lib/components/HeroSection/HeroSection.svelte';
-	import { gsap } from 'gsap';
-
-	function moveBox(e: MouseEvent) {
-		gsap.to('#mouse-trail', {
-			duration: 0.35,
-			x: e.pageX,
-			y: e.pageY,
-			ease: 'cubic-bezier(0.26, 0.9, 0.5, 1)' // Example easing function, change as needed
-		});
-	}
 </script>
 
-<div on:mousemove={moveBox} role="presentation">
+<div>
 	<HeroSection />
 	<div style="height: 60vh;" class="bg-red-400 porfolioSection relative" />
 	<div style="height: 40vh;" id="" class="bg-blue-400 relative" />
