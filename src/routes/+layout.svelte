6ad26d9dbbe3fa335c42b02f67eeea869105ed00
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-03-25 11:48:53 -0500

    add splash screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import SplashScreen from '$lib/components/SplashScreen/SplashScreen.svelte';
+
+	let showSplash = true;
+
+	// Listen for the custom event to hide the splash screen
+	onMount(() => {
+		window.addEventListener('hide-splash', () => {
+			showSplash = false;
+		});
+	});
 </script>
 
-<slot />
+{#if showSplash}
+	<SplashScreen />
+{:else}
+	<slot />
+{/if}
