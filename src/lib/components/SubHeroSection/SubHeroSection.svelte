eeb2a20f97d75b423a7063c1faf85c0853cf5aa7
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-03-26 17:58:12 -0500

    enter animation for subhero completed

--- src/lib/components/SubHeroSection/SubHeroSection.svelte
+++ src/lib/components/SubHeroSection/SubHeroSection.svelte
@@ -7,51 +7,60 @@
 		const scrollTrigger = {
 			trigger: '#subheroSection',
 			start: 'top top',
-			end: '+=100vh', // End trigger 100vh (viewport height) from the top of the trigger
-			pin: true,
-			markers: true,
+			end: '+=200', // End trigger 100vh (viewport height) from the top of the trigger
+			markers: false,
 			scrub: true
 		};
-		// timeline
-		// 	.fromTo(
-		// 		'.right',
-		// 		{
-		// 			x: '100vw'
-		// 		},
-		// 		{
-		// 			x: 0,
-		// 			scrollTrigger
-		// 		}
-		// 	)
-		// 	.fromTo(
-		// 		'.left',
-		// 		{
-		// 			x: '-100vw'
-		// 		},
-		// 		{
-		// 			x: 0,
-		// 			scrollTrigger
-		// 		}
-		// 	)
-		// 	.fromTo(
-		// 		'.heroCaption',
-		// 		{
-		// 			scale: 0.98,
-		// 			opacity: 0
-		// 		},
-		// 		{
-		// 			scale: 1,
-		// 			opacity: 1,
-		// 			scrollTrigger
-		// 		}
-		// 	);
+		timeline
+			.fromTo(
+				'.right',
+				{
+					x: '100vw'
+				},
+				{
+					x: 0,
+					scrollTrigger
+				}
+			)
+			.fromTo(
+				'.left',
+				{
+					x: '-100vw'
+				},
+				{
+					x: 0,
+					scrollTrigger
+				}
+			)
+			.fromTo(
+				'.heroCaption',
+				{
+					scale: 0.98,
+					opacity: 0,
+					y: 200
+				},
+				{
+					scale: 1,
+					opacity: 1,
+					y: 0,
+					scrollTrigger: { ...scrollTrigger, pin: true }
+				}
+			);
 	});
 </script>
 
 <section id="subheroSection" class="h-svh w-svw overflow-hidden">
 	<div id="subheroInner" class="flex flex-col justify-center my-auto w-full h-full">
-		<h2 class="playfair font-semibold text-right left subheading">CREATIVE WEB</h2>
-		<h2 class="playfair font-semibold text-right right subheading">DEVELOPER</h2>
+		<h2
+			class="playfair opacity-100 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/100 to-white/50 font-semibold text-right left subheading"
+		>
+			CREATIVE WEB
+		</h2>
+		<h2
+			class="playfair opacity-100 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/100 to-white/50 font-semibold text-right right subheading"
+		>
+			DEVELOPER
+		</h2>
 		<h4 class="playfair font-medium text-center text-2xl heroCaption sm:w-1/3 mx-auto mt-20">
 			Transforming tomorrow with tech-infused narratives, my software creates fair and fun
 			experiences for all.
