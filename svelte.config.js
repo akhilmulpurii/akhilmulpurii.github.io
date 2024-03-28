87970a779ebbc8529fe19deb087d5323b486bf1f
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-03-28 11:06:47 -0500

    comit gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,27 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'/',
+				'/blog',
+				'/contact',
+				'/projects',
+				'/projects/noisefit',
+				'/projects/heyhey',
+				'/projects/frankdarling',
+				'/projects/skymd',
+				'/projects/justact',
+				'/projects/hxouse',
+				'/projects/winekey',
+				'/projects/pharmaclock',
+				'/projects/healthapp',
+				'/projects/my-visitly',
+				'/projects/punchapp',
+				'/projects/rangde'
+			]
+		}
 	}
 };
 
