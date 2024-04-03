6663367946aacb2597c1f9635194707c567d9796
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-04-03 10:33:41 -0500

    basic animation

--- src/lib/components/ProjectDetailHeroSection/ProjectDetailHeroSection.svelte
+++ src/lib/components/ProjectDetailHeroSection/ProjectDetailHeroSection.svelte
@@ -8,33 +8,93 @@
 	export let timeline: gsap.core.Timeline;
 
 	onMount(() => {
-		timeline.fromTo(
-			'#heroNav',
-			{
-				opacity: 0.5,
-				duration: 0.5
-			},
-			{
-				y: -100,
-				scrollTrigger: {
-					trigger: '#projectHeroSection',
-					start: 'top 50',
-					scrub: true
+		timeline
+			.fromTo(
+				'#heroNav',
+				{
+					opacity: 0.5,
+					duration: 0.5
+				},
+				{
+					y: -100,
+					scrollTrigger: {
+						trigger: '#projectHeroSection',
+						start: 'top 50',
+						scrub: true
+					}
 				}
-			}
-		);
+			)
+			.fromTo(
+				'#projectLogo',
+				{
+					opacity: 1,
+					scale: 1,
+					y: 0
+				},
+				{
+					opacity: 0,
+					scale: 0.9,
+					y: -50,
+
+					scrollTrigger: {
+						trigger: '#projectHeroSection',
+						start: 'top 0',
+						scrub: true
+					}
+				}
+			)
+			.fromTo(
+				'#projectYear',
+				{
+					opacity: 1,
+					scale: 1,
+					y: 0
+				},
+				{
+					opacity: 0,
+					scale: 0.9,
+					y: -50,
+					scrollTrigger: {
+						trigger: '#projectHeroSection',
+						start: 'top 0',
+						scrub: true
+					}
+				}
+			)
+			.fromTo(
+				'#projectShortDesc',
+				{
+					opacity: 1,
+					scale: 1,
+					y: 0
+				},
+				{
+					opacity: 0,
+					scale: 0.9,
+					y: -50,
+					scrollTrigger: {
+						trigger: '#projectHeroSection',
+						start: 'top 0',
+						scrub: true
+					}
+				}
+			);
 	});
 </script>
 
 <section id="projectHeroSection" class="h-[50vh] w-svw flex flex-col items-center justify-between">
 	<Header />
 	<div class="flex flex-col items-center space-y-5">
-		<img src={project.logo} alt="logo" class="h-10 invert" />
-		<span class="text-gray-500 text-sm font-medium orbitron tracking-wider uppercase">
+		<img id="projectLogo" src={project.logo} alt="logo" class="h-10 invert" />
+		<span
+			id="projectYear"
+			class="text-gray-500 text-sm font-medium orbitron tracking-wider uppercase"
+		>
 			{project.organizaton} - {project.year}
 		</span>
 	</div>
 	<h1
+		id="projectShortDesc"
 		class="text-6xl w-1/2 text-center font-semibold opacity-100 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-black/100 to-black/80"
 	>
 		{project.short_description}
