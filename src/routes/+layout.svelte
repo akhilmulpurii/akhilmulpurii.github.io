77ac48abd9d0da1e5e36bb88d605a817d0c88b51
Akhil Mulpuri <akhilfilms0@gmail.com>
2024-07-08 02:33:40 -0500

    remove splash screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,14 @@
 <script>
 	import '../app.css';
 	import { onMount } from 'svelte';
-	import SplashScreen from '$lib/components/SplashScreen/SplashScreen.svelte';
 	import Noise from '$lib/assets/noise.png';
 	import Lenis from '@studio-freight/lenis';
 	import { gsap } from 'gsap';
 	import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
-	let showSplash = true;
-
 	// Listen for the custom event to hide the splash screen
 
 	onMount(() => {
-		window.addEventListener('hide-splash', () => {
-			showSplash = false;
-		});
-
 		gsap.registerPlugin(ScrollTrigger);
 
 		const lenis = new Lenis();
@@ -33,9 +26,4 @@
 		style="background: url({Noise})"
 		class="inset-0 w-screen h-screen opacity-40 fixed top-0 left-0 right-0 bottom-0 z-[1] pointer-events-none"
 	/>
-	{#if showSplash}
-		<SplashScreen />
-	{:else}
-		<slot />
-	{/if}
 </div>
