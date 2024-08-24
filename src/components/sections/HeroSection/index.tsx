3a2e36d77f3cb225d14b297147dae162e6eef85c
Akhil <akhilfilms02@gmail.com>
2024-08-24 13:06:08 -0500

    add repeat animation

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { AuroraBackground } from "@/components/ui/AuroraBackground";
 import { Libre_Baskerville, Bricolage_Grotesque } from "next/font/google";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { RefObject, useRef } from "react";
 import clsx from "clsx";
 
 const LibreBaskerville = Libre_Baskerville({
@@ -19,11 +20,19 @@ const BricolageGrotesque = Bricolage_Grotesque({
 interface HeroSectionProps {}
 
 const HeroSection: React.FunctionComponent<HeroSectionProps> = () => {
+  const ref1: RefObject<HTMLHeadingElement> = useRef(null);
+  const ref2: RefObject<HTMLHeadingElement> = useRef(null);
+
+  // Adjust `amount` and `once` options
+  const inView1 = useInView(ref1, { once: false, amount: "some" });
+  const inView2 = useInView(ref2, { once: false, amount: "some" });
+
   return (
     <AuroraBackground>
       <motion.h1
+        ref={ref1}
         initial={{ x: 100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
+        animate={{ x: inView1 ? 0 : 100, opacity: inView1 ? 1 : 0 }}
         transition={{
           type: "spring",
           stiffness: 260,
@@ -37,8 +46,9 @@ const HeroSection: React.FunctionComponent<HeroSectionProps> = () => {
         Building the <span className="text-primary">Future</span>
       </motion.h1>
       <motion.h1
+        ref={ref2}
         initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
+        animate={{ x: inView2 ? 0 : -100, opacity: inView2 ? 1 : 0 }}
         transition={{
           type: "spring",
           stiffness: 260,
