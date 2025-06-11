434a17e25ad75af6a296e72ffef0e62414375b27
Akhil <akhilfilms02@gmail.com>
2025-06-11 10:30:30 -0500

    fix design for mobile view

--- src/lib/components/HeroSection/HeroSection.svelte
+++ src/lib/components/HeroSection/HeroSection.svelte
@@ -44,12 +44,11 @@
 
 <section id="heroSection" class="h-svh w-svw overflow-hidden">
 	<div class="heroTrigger h-full w-full">
-		<div id="heroInnerDiv" class="flex flex-col h-full w-full items-center justify-between">
+		<div id="heroInnerDiv" class="flex flex-col h-full w-full items-center md:justify-between">
 			<Header />
-			<div id="hero-title" class="flex flex-col justify-center items-center">
+			<div id="hero-title" class="flex flex-col h-[100vh] justify-center items-center">
 				<h1
-					style="font-size:33vw;line-height:33vw;"
-					class="select-none playfair z-[2] font-bold text-center opacity-100 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-black/100 to-black/80"
+					class="select-none playfair z-[2] text-[30vw] md:text-[33vw] leading-[33vw] font-bold text-center opacity-100 bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-black/100 to-black/80"
 				>
 					AKHIL
 				</h1>
