5787a9d44b22cf8b0d53ed54788b257020925e92
Akhil <akhilfilms02@gmail.com>
2025-06-11 10:59:21 -0500

    fix about section styles to accomodate bento style grid for better styling for mobile view

--- src/lib/components/AboutMeSection/AboutMeSection.svelte
+++ src/lib/components/AboutMeSection/AboutMeSection.svelte
@@ -6,7 +6,7 @@
 	import { gsap } from 'gsap';
 
 	const filter =
-		'brightness-[105%] grayscale-[100%] contrast-[100%] hue-rotate-0 invert-0 opacity-100 saturate-[100%] sepia-[15%]';
+		'brightness-[105%] grayscale-[100%] contrast-[100%] hue-rotate-0 invert-0 opacity-100 saturate-[100%] sepia-[20%]';
 
 	export let timeline: gsap.core.Timeline;
 
@@ -58,7 +58,7 @@
 						scrollTrigger: {
 							trigger: container,
 							start: 'top 50%',
-							end: 'bottom 50%',
+							end: 'bottom 60%',
 							scrub: true,
 							pin: false
 						}
@@ -88,34 +88,35 @@
 	});
 </script>
 
-<section class="w-svw flex items-center justify-center h-[80vh]">
+<section class="w-svw flex items-center justify-center md:h-[80vh]">
 	<div
-		class="playfair img-container w-full h-full p-8 relative overflow-hidden flex flex-col items-start justify-center"
+		class="playfair img-container w-full h-full p-8 relative overflow-hidden flex flex-col items-center md:items-start justify-center"
 	>
 		<h1
 			id="about-title"
-			class="z-10 text-[38px] leading-[38px] tracking-[-1.4px] uppercase text-left font-normal w-3/4 text-white"
+			class="z-10 text-[32px] leading-[32px] md:text-[38px] md:leading-[38px] tracking-[-1.4px] uppercase text-left font-normal w-full md:w-3/4 text-white"
 		>
 			I am a designer and website developer with a creative approach and an analytical mindset.
 		</h1>
-		<div class="z-10 grid mt-5 grid-cols-4 gap-6">
-			<span />
-			<span class="about-caption text-xl text-white"
-				>Designing websites that solve, captivate, and convert.</span
-			>
-			<span class="about-caption text-xl text-white">
-				Integrating analysis with creativity, I craft websites that solve tasks and inspire
-				engagement.
+		<div class="z-10 grid mt-6 md:mt-16 grid-cols-[repeat(12,minmax(0,1fr))] grid-flow-dense gap-6">
+			<span class="hidden md:block md:col-span-3" />
+			<span class="about-caption text-xl text-white col-span-12 md:col-span-3">
+				<span class="md:hidden">&#10238; </span>Designing websites that solve, captivate, and
+				convert.
 			</span>
-			<span />
-			<span />
+			<span class="about-caption text-xl text-white col-span-12 md:col-span-3">
+				<span class="md:hidden">&#10238; </span>Integrating analysis with creativity, I craft
+				websites that solve tasks and inspire engagement.
+			</span>
+			<span class="hidden md:block md:col-span-3" />
+			<span class="hidden md:block md:col-span-3" />
 			<img
-				class="{filter} about-tile-img aspect-square object-cover object-right-bottom"
+				class="{filter} about-tile-img aspect-square object-cover object-right-bottom col-span-6 md:col-span-3"
 				src={Portrait}
 				alt="portrait"
 			/>
 			<img
-				class="{filter} about-tile-img aspect-square object-top object-cover"
+				class="{filter} about-tile-img aspect-square object-top object-cover col-span-6 md:col-span-3"
 				src={Temple}
 				alt="temple"
 			/>
@@ -123,7 +124,7 @@
 		</div>
 		<img
 			id="parallax-img"
-			class="w-full object-top {filter} z-0 h-full absolute top-0 left-1/2 object-cover -translate-x-1/2 scale-150"
+			class="w-full object-top {filter} blur-[1px] md:blur-none z-0 h-full absolute top-0 left-1/2 object-cover -translate-x-1/2 scale-150"
 			alt="pict"
 			src={Photography}
 		/>
