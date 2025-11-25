50a7c8d617e39fdc400c85b14dcfe9c9e0282422
S.A.M <akhilfilms02@gmail.com>
2025-11-25 00:01:07 -0600

    remove entire codebase for nextjs migration

--- src/routes/blog/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { getBlogPosts } from '$lib/sanityClient';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	return { blogPosts: await getBlogPosts() };
-};
