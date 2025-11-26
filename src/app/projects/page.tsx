1774478ce420b8227ed1e96adb003608bb827c97
S.A.M <akhilfilms02@gmail.com>
2025-11-25 19:00:42 -0600

    add footer section

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
 import { DockNavigation } from "@/components/sections/dock-navigation";
+import { FooterSection } from "@/components/sections/footer-section";
 import ProjectHeroSection from "@/components/sections/project-hero-section";
 const ProjectPage = () => {
   return (
     <div>
       <ProjectHeroSection />
+      <FooterSection />
       <DockNavigation />
     </div>
   );
